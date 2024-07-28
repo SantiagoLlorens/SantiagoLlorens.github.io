5d276f7203298bec0b119379b099e9e02932548c
Santiago Llorens <60903375+SantiagoLlorens@users.noreply.github.com>
2024-07-28 20:15:34 +0200

    s

--- _talks/talkmap.py
+++ _talks/talkmap.py
@@ -18,7 +18,7 @@ from geopy import Nominatim
 g = glob.glob("*.md")
 
 
-geocoder = Nominatim(user_agent="yagollorensf@gmail.com")
+geocoder = Nominatim(user_agent="@gmail.com")
 location_dict = {}
 location = ""
 permalink = ""
